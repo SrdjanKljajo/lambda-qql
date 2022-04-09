ea0ae3c8988d073107b2769548f7b68363fc7d3c
Srdjan <websmartist@gmail.com>
2022-04-09 14:36:20 +0200

    server error 502 finish36

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/* const { join } = require(`path`)
 const slsw = require(`serverless-webpack`)
 const nodeExternals = require(`webpack-node-externals`)
 
@@ -6,9 +7,32 @@ module.exports = {
   entry: slsw.lib.entries,
   target: `node`,
   externals: [nodeExternals()],
+  devtool: `nosources-source-map`,
+  output: {
+    libraryTarget: `commonjs`,
+    path: join(__dirname, `.webpack`),
+    filename: `[name].js`,
+    sourceMapFilename: `[file].map`
+  },
   stats: {
     colors: true,
     reasons: false,
     chunks: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: `babel-loader`,
+        exclude: /node_modules/,
+        options: {
+          cacheDirectory: true
+        }
+      },
+      {
+        test: /\.(graphql|gql)$/,
+        exclude: /node_modules/
+      }
+    ]
   }
-}
+} */
