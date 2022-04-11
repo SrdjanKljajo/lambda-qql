5f4c3dce34f65c42495d49f7e421572b7a5ed923
Srdjan <websmartist@gmail.com>
2022-04-11 17:10:42 +0200

    Size of lambda without serverless- webpack

--- lambdas/graphql.js
+++ lambdas/graphql.js
@@ -1,18 +1,6 @@
-const { ApolloServer, gql } = require('apollo-server-lambda');
-
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-// Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-  },
-};
+const { ApolloServer } = require('apollo-server-lambda');
+const typeDefs = require('./src/schema')
+const resolvers = require('./src/resolver')
 
 const server = new ApolloServer({
   typeDefs,
